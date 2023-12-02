2f69c2b974903d224f3adbeb1cb5330350448b86
ravine13 <kuriaderek@gmail.com>
2023-12-02 17:58:11 +0300

    passed test

--- lib/file_io.py
+++ lib/file_io.py
@@ -1,8 +1,12 @@
 def write_file(file_name, file_content):
-    pass
+    with open(f'{file_name}.txt',mode="w") as file:
+        file.write(file_content)
 
 def append_file(file_name, append_content):
-    pass
+    with open(f'{file_name}.txt',mode='a') as file:
+        file.write(append_content)
 
 def read_file(file_name):
-    pass
+    with open(f'{file_name}.txt') as file:
+        content = file.read()
+        return content
